e0a2cc842629aff7e36e02f1fcba303bcd7ab81e
Felix Lu <f33losopher@gmail.com>
2011-09-18 23:16:30 -0700

    Weather Station

---
+++ WeatherStation/src/WeatherStation.java
@@ -0,0 +1,17 @@
+
+public class WeatherStation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		WeatherData weatherData = new WeatherData();
+		
+		CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+		
+		weatherData.setMeasurements(80, 65, 30.4f);
+		weatherData.setMeasurements(82, 70, 29.2f);
+		weatherData.setMeasurements(78, 90, 29.2f);
+	}
+
+}
