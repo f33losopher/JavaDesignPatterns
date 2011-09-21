2ed04de719969ab7a72892cae7aab3b881b068bb
Felix Lu <f33losopher@gmail.com>
2011-09-21 00:13:06 -0700

    Java IO Decorator

---
+++ JavaIODecorator/src/InputTest.java
@@ -0,0 +1,31 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest
+{
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    int c;
+    try
+    {
+      InputStream in = new LowerCaseInputStream(
+          new BufferedInputStream(
+              new FileInputStream("src/test.txt")));
+
+      while ((c = in.read()) >= 0)
+      {
+        System.out.print((char) c);
+      }
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
