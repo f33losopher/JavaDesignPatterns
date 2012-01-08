b3d46cec173f3b0b4fcd235b0661dfa437ecd578
Felix Lu <f33losopher@gmail.com>
2012-01-08 13:34:53 -0800

    Command Pattern

--- Command/src/Receiver/CeilingFan.java
+++ Command/src/Receiver/CeilingFan.java
@@ -2,20 +2,46 @@ package Receiver;
 
 public class CeilingFan
 {
-   private String name;
-   
-   public CeilingFan(String name)
+   public static final int HIGH   = 3;
+   public static final int MEDIUM = 2;
+   public static final int LOW    = 1;
+   public static final int OFF    = 0;
+   private String          location;
+   private int             speed;
+
+   public CeilingFan(String location)
    {
-      this.name = name;
+      this.location = location;
+      speed = OFF;
+   }
+
+   public void high()
+   {
+      speed = HIGH;
+   }
+   
+   public void medium()
+   {
+      speed = MEDIUM;
+   }
+   
+   public void low()
+   {
+      speed = LOW;
+   }
+
+   public void off()
+   {
+      speed = OFF;
    }
    
    public void on()
    {
-      System.out.println(name + " Ceiling Fan on");
+      System.out.println("Ceiling Fan: " + location + " is on");
    }
    
-   public void off()
+   public int getSpeed()
    {
-      System.out.println(name + " Ceiling Fan off");
+      return speed;
    }
 }
